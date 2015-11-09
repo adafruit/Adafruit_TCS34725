02b0f2a3bb58e8638db8ac83a4a505e3265be2e2
JaKasb <jakob@kasbauer.eu>
2015-11-09 13:41:46 +0100

    Corrected gain value in comment

--- Adafruit_TCS34725.h
+++ Adafruit_TCS34725.h
@@ -109,7 +109,7 @@ tcs34725IntegrationTime_t;
 typedef enum
 {
   TCS34725_GAIN_1X                = 0x00,   /**<  No gain  */
-  TCS34725_GAIN_4X                = 0x01,   /**<  2x gain  */
+  TCS34725_GAIN_4X                = 0x01,   /**<  4x gain  */
   TCS34725_GAIN_16X               = 0x02,   /**<  16x gain */
   TCS34725_GAIN_60X               = 0x03    /**<  60x gain */
 }
