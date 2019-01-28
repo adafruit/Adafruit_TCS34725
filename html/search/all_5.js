b72ac229e9fe079ebe2953a88bbc5b40350bb0fe
Travis CI <travis@travis-ci.org>
2019-01-28 19:37:35 +0000

    Deploy code docs to GitHub Pages Travis build: 11
    Commit: fc723bf5a5325bbf20a2370a242f79167bb2bc40

--- html/search/all_5.js
+++ html/search/all_5.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['getrawdata',['getRawData',['../class_adafruit___t_c_s34725.html#abd9946a9baab1e0c76248cfe1864ea27',1,'Adafruit_TCS34725']]],
-  ['getrawdataoneshot',['getRawDataOneShot',['../class_adafruit___t_c_s34725.html#a25e1eb0a66a2b0a5948ab4eac4ccb4c9',1,'Adafruit_TCS34725']]]
+  ['getrawdataoneshot',['getRawDataOneShot',['../class_adafruit___t_c_s34725.html#a25e1eb0a66a2b0a5948ab4eac4ccb4c9',1,'Adafruit_TCS34725']]],
+  ['getrgb',['getRGB',['../class_adafruit___t_c_s34725.html#a00117af3b2ae21c8597e1e72d9171fe1',1,'Adafruit_TCS34725']]]
 ];
